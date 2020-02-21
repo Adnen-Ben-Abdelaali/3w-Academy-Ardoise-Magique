80d05374396eb6db0341dd7b03e736825e48f09d
Adnen-Ben-Abdelaali <benabdelaaliadnen@gmail.com>
2020-02-21 14:44:16 +0100

    version procédurale achevée -- fonctionnalité 'palette de couleurs ' non initialisée

--- js/main.js
+++ js/main.js
@@ -6,7 +6,8 @@ let ctx = canv.getContext("2d");
 
 let drawing = false;
 let positionInitiale = {};
-
+let colorPicker = new Array();
+let epaisseurTrai = new Array();
 ctx.lineWidth = "15";
 ctx.strokeStyle = "black";
 
@@ -20,36 +21,29 @@ canv.addEventListener("mouseup", onMouseUp);
 /** gestionnaire des évènements de "Click" sur les boutons 
  de changement d'épaisseur du trait **/
 
- let traitFin = document.getElementById("traitFin");
- let traitNormal = document.getElementById("traitNormal");
- let traitEpais = document.getElementById("traitEpais");
- 
- 
- 
- traitFin.addEventListener("click", onClickTraitFin);
- traitNormal.addEventListener("click", onCLickTraitNormal);
- traitEpais.addEventListener("click", onClickTraitEpais);
- 
- /** gestionnaire des événements pemettant 
-	le changment de couleur lors du drawing dans le canvas **/
- 
-	let blackColor = document.getElementById("blackColor");
-	let brownColor = document.getElementById("brownColor");
-	let redColor = document.getElementById("redColor");
-	let orangeColor = document.getElementById("orangeColor"); 
-	let greenColor = document.getElementById("greenColor");
-	let vermisselleColor = document.getElementById("vermisselleColor");
-	let greenBlueColor = document.getElementById("greenBlueColor");
- 
- 
- blackColor.addEventListener("click", onClickBlackColor);
- brownColor.addEventListener("click", onClickBrownColor); 
- redColor.addEventListener("click", onClickRedColor);
- orangeColor.addEventListener("click", onClickOrangeColor); 
- greenColor.addEventListener("click", onClickGreenColor);
- vermisselleColor.addEventListener("click", onClickVermisselleColor);
- greenBlueColor.addEventListener("click", onClickBlueColor); 
- 
+let boutonEpaisseurTrais = document.getElementsByClassName("boutonEpaisseurTrais");
+
+console.log(boutonEpaisseurTrais);
+
+for(let elt of boutonEpaisseurTrais)
+{
+	elt.addEventListener("click", onClickBoutonEpaisseurTrais);
+}
+
+/** gestionnaire des événemets permettant le choix de la couleur 
+ de dessin dans le canvas lors du click **/
+
+ let boutonListeCouleur = document.getElementsByClassName("boutonListeCouleur");
+  
+ console.log(boutonListeCouleur);
+
+ for(let element of boutonListeCouleur) {
+		console.log(element);
+		element.addEventListener("click", onClickBoutonListeCouleur);
+	 
+ }
+
+  
  /** gestionnaire des événements  permettant
 	 l'effacement du contenu dessiné dans la balise canvas **/
  
@@ -65,4 +59,5 @@ canv.addEventListener("mouseup", onMouseUp);
  
  eyeDropper.addEventListener("click", onClickEyeDropper);
  
+
  
